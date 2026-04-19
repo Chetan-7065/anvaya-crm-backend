05428e5667a0fc1fcb6f1e60f1f0fbea95c4a00c
unknown <your.email@example.com>
2026-04-19 12:38:45 +0530

    fix(db): increase socketTimeoutMS and connectTimeoutMS for mongoose

--- db/db.connect.js
+++ db/db.connect.js
@@ -5,7 +5,14 @@ const mongoUri = process.env.MONGODB
 
 const initializeDatabase = () => {
   mongoose
-  .connect(mongoUri)
+  .connect(mongoUri, {
+    maxPoolSize: 20,
+    serverSelectionTimeoutMS: 30000,
+    socketTimeoutMS: 45000,
+    connectTimeoutMS: 30000, 
+    retryWrites: true,    
+    retryReads: true
+  })
   .then(() => {
     console.log("Connected Successfully")
   }).catch((error) => {
